36279a688819a9add7a063a58a749e146c768a18
padamdahal <dahal0514@gmail.com>
2019-01-22 12:46:25 +0545

    Update dhis-custom-form-date-conversion.js
    Update for event capture

--- dhis-custom-form-date-conversion.js
+++ dhis-custom-form-date-conversion.js
@@ -21,18 +21,18 @@ $(document).ready(function(){
 	});
 	
 	// Date of admission - Event
-	$("d2-custom-registration-form input[name=SDZ8k24XOtR]").calendarsPicker({
+	$("#tTQr7cLOXOi-SDZ8k24XOtR-val").calendarsPicker({
 		calendar: $.calendars.instance('nepali'),
 		dateFormat: 'yyyy-mm-dd',
 		onSelect: function(dates) {
-			$("d2-custom-registration-form input[name=SDZ8k24XOtR]").trigger("change");
-			$("d2-custom-registration-form input[name=SDZ8k24XOtR]").trigger("blur");
+			$("#tTQr7cLOXOi-SDZ8k24XOtR-val").trigger("change");
+			$("#tTQr7cLOXOi-SDZ8k24XOtR-val").trigger("blur");
 			var bsConvertor = new BikramSambatConverter();
 			var npDate = String(dates);
 			var enDate = bsConvertor.nep_to_eng(npDate.substring(0,4), npDate.substring(5,7), npDate.substring(8,10));
-			$("body input[name=eventDate]").val(enDate.year+"-"+enDate.month+"-"+enDate.date);
-			$("body input[name=eventDate]").trigger("change");
-			$("body input[name=eventDate]").trigger("blur");
+			$(".hasCalendarsPicker").val(enDate.year+"-"+enDate.month+"-"+enDate.date);
+			$("hasCalendarsPicker").trigger("change");
+			$("hasCalendarsPicker").trigger("blur");
 			$('.ui-datepicker-cmd-close').trigger("click");
 		}
 	});
