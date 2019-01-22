4c79f5d7201a554eb9af83c8c43d7d2506069509
filoi <neeraj@filoi.in>
2019-01-22 11:37:06 +0530

    Update dhis-custom-form-date-conversion.js
    Nepali Date (Date of birth) Event changes testing - 3

--- dhis-custom-form-date-conversion.js
+++ dhis-custom-form-date-conversion.js
@@ -58,21 +58,22 @@ $(document).ready(function(){
 	});
 	
 		// Date of birth - event
-	$("d2-custom-registration-form input[name=tTQr7cLOXOi-o1wH0CNAwiI-val]").calendarsPicker({
+	$("d2-custom-data-entry-form #tTQr7cLOXOi-o1wH0CNAwiI-val").calendarsPicker({
 		calendar: $.calendars.instance('nepali'),
 		yearRange: '-120:+30',
 		duration: "fast",
 	    showAnim: "",
 		dateFormat: 'yyyy-mm-dd',
 		onSelect: function(dates) {
-			$("d2-custom-registration-form input[name=tTQr7cLOXOi-o1wH0CNAwiI-val]").trigger("change");
-			$("d2-custom-registration-form input[name=tTQr7cLOXOi-o1wH0CNAwiI-val]").trigger("blur");
+			$("d2-custom-data-entry-form #tTQr7cLOXOi-o1wH0CNAwiI-val").trigger("change");
+			$("d2-custom-data-entry-form #tTQr7cLOXOi-o1wH0CNAwiI-val").trigger("blur");
 			var bsConvertor = new BikramSambatConverter();
 			var npDate = String(dates);
 			var enDate = bsConvertor.nep_to_eng(npDate.substring(0,4), npDate.substring(5,7), npDate.substring(8,10));
-			$("d2-custom-registration-form input[name=tTQr7cLOXOi-WXHUaOqMYde-val]").val(enDate.year+"-"+enDate.month+"-"+enDate.date);
-			$("d2-custom-registration-form input[name=tTQr7cLOXOi-WXHUaOqMYde-val]").trigger("change");
-			$("d2-custom-registration-form input[name=tTQr7cLOXOi-WXHUaOqMYde-val]").trigger("blur");
+			$("d2-custom-data-entry-form #tTQr7cLOXOi-WXHUaOqMYde-val").val(enDate.year+"-"+enDate.month+"-"+enDate.date);
+			$("d2-custom-data-entry-form #tTQr7cLOXOi-WXHUaOqMYde-val").trigger("change");
+			$("d2-custom-data-entry-form #tTQr7cLOXOi-WXHUaOqMYde-val").trigger("blur");
+			
 			$('.ui-datepicker-cmd-close').trigger("click");
 		}
 	});
