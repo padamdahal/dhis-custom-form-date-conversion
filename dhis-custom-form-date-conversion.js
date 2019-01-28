042b00676b83260db6a48137c5611a0c4b5d17c7
filoi <neeraj@filoi.in>
2019-01-28 10:24:29 +0530

    Update dhis-custom-form-date-conversion.js
    Event Date (Nepali Calendar change)- Test1

--- dhis-custom-form-date-conversion.js
+++ dhis-custom-form-date-conversion.js
@@ -21,12 +21,15 @@ $(document).ready(function(){
 	});
 	
 	// Date of admission - Event
-	$("#tTQr7cLOXOi-SDZ8k24XOtR-val").calendarsPicker({
+	$("d2-custom-data-entry-form #tTQr7cLOXOi-SDZ8k24XOtR-val").calendarsPicker({
 		calendar: $.calendars.instance('nepali'),
+		yearRange: '-120:+30',
+		duration: "fast",
+	    showAnim: "",
 		dateFormat: 'yyyy-mm-dd',
 		onSelect: function(dates) {
-			$("#tTQr7cLOXOi-SDZ8k24XOtR-val").trigger("change");
-			$("#tTQr7cLOXOi-SDZ8k24XOtR-val").trigger("blur");
+			$("d2-custom-data-entry-form #tTQr7cLOXOi-SDZ8k24XOtR-val").trigger("change");
+			$("d2-custom-data-entry-form #tTQr7cLOXOi-SDZ8k24XOtR-val").trigger("blur");
 			var bsConvertor = new BikramSambatConverter();
 			var npDate = String(dates);
 			var enDate = bsConvertor.nep_to_eng(npDate.substring(0,4), npDate.substring(5,7), npDate.substring(8,10));
